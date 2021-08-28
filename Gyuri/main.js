fc370c8be1a23977d813bac90dedc854e84755d7
Gyuri <matrai2012@gmail.com>
2021-08-28 23:26:51 +0200

    2.0

--- Gyuri/main.js
+++ Gyuri/main.js
@@ -9,34 +9,33 @@ async function getWeather() {
     console.log("Adatok feldolgozasahoz:", data); 
 
     let city_name = document.querySelector(".city");
-    city_name.innerHTML += data.name;
+    city_name.innerHTML = data.name;
 
     let homerseklet = document.querySelector(".temperature");
-    homerseklet.innerHTML += Math.round(data.main.temp -0) + " °C ";
+    homerseklet.innerHTML = Math.round(data.main.temp -0) + " °C ";
 
     let weather = document.querySelector(".weathertext");
     let text = data.weather[0].description[0].toUpperCase();
-    weather.innerHTML += text + data.weather[0].description.slice(1); 
+    weather.innerHTML = text + data.weather[0].description.slice(1); 
  
     let tempmin = document.querySelector(".tempmin");
-    tempmin.innerHTML += Math.round(data.main.temp_min) + " °C ";
+    tempmin.innerHTML = Math.round(data.main.temp_min) + " °C ";
 
 
     let tempmax = document.querySelector(".tempmax");
-    tempmax.innerHTML += Math.round(data.main.temp_max) + " °C ";
+    tempmax.innerHTML = Math.round(data.main.temp_max) + " °C ";
 
 
     let humidity = document.querySelector(".humidity");
-    humidity.innerHTML += (data.main.humidity)
+    humidity.innerHTML = data.main.humidity + " %";
 
 
     let pressure = document.querySelector(".pressure");
-    pressure.innerHTML += (data.main.pressure)
+    pressure.innerHTML = data.main.pressure + " hPh";
 
 
 
 }
-getWeather();
+getWeather(); 
 
-pressure
-humidity
+
